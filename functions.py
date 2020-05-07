d1eb8172a09e51f06c28c1b9e47ce59947b22e3a
Cristian Ignacio Araujo <cristianaraujoaraujo@gmail.com>
2020-05-07 19:26:12 -0400

    Se agregan funciones de multiplicación y división

--- functions.py
+++ functions.py
@@ -8,3 +8,12 @@ def suma(num1, num2):
 
 def resta(num1, num2):
     return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if (num1 != 0):
+        return num1/num2
+    else:
+        return 'No se puede dividir por 0'
